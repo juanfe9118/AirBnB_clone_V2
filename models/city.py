fd4d052ab58221bbab00a49f161be384d34cd246
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-03 02:52:56 +0000

    Implemented DBStorage engine on Place class (and modified User and City classes to comply with those changes)

--- models/city.py
+++ models/city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This is the city class"""
+from models.place import Place
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -13,3 +15,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
