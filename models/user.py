022da21b479574614c2407fd79d870f8aeff0809
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-03 05:44:36 +0000

    PEP8 Fix: Overindentation during visual indent

--- models/user.py
+++ models/user.py
@@ -23,4 +23,4 @@ class User(BaseModel, Base):
     places = relationship('Place', backref='user',
                           cascade='all, delete-orphan')
     reviews = relationship('Review', backref='user',
-                            cascade='all, delete-orphan')
+                           cascade='all, delete-orphan')
