0cf35eca723bad88d916754069416594b5dcbc03
juanfe9118 <1168@holbertonschool.com>
2020-04-11 01:17:58 -0500

    Fix pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,8 @@ from datetime import datetime
 def do_pack():
     """Packs all files in the web_static directory using tar"""
     print("Packing web_static to versions/web_static_20170314233357.tgz")
-    if not exists('versions') or (exists('versions') and not isdir('versions')):
+    if not exists('versions') or (exists('versions') and
+                                  not isdir('versions')):
         local('mkdir -p versions')
     date = datetime.now()
     command = 'tar -cvzf versions/web_static_'
