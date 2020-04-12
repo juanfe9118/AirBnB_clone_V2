640546316b64860d5d5523b17e936c3c0b324ee3
juanfe9118 <1168@holbertonschool.com>
2020-04-11 23:27:42 -0500

    Fix print in pack and add deploy script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,15 +8,18 @@ from datetime import datetime
 
 def do_pack():
     """Packs all files in the web_static directory using tar"""
-    print("Packing web_static to versions/web_static_20170314233357.tgz")
     if not exists('versions') or (exists('versions') and
                                   not isdir('versions')):
         local('mkdir -p versions')
     date = datetime.now()
-    command = 'tar -cvzf versions/web_static_'
-    command += '{:4}{:02}{:02}'.format(date.year, date.month, date.day)
-    command += '{:02}{:02}{:02}'.format(date.hour, date.minute, date.second)
-    command += '.tgz web_static'
+    command = 'tar -cvzf '
+    filename = 'versions/web_static_'
+    filename += '{:4}{:02}{:02}'.format(date.year, date.month, date.day)
+    filename += '{:02}{:02}{:02}'.format(date.hour, date.minute, date.second)
+    filename += '.tgz'
+    command += filename
+    command += ' web_static'
+    print("Packing web_static to {}".format(filename))
     try:
         local(command)
     except:
